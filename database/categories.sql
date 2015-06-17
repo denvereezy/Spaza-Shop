56b77d747203b69dbf3cc7c9ca258763b3fa8e81
denver <denver@projectcodex.co>
2015-06-17 16:17:14 +0200

    database

---
+++ database/categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO Categories (Name) VALUES ('Dairy');
+INSERT INTO Categories (Name) VALUES ('Bakery');
+INSERT INTO Categories (Name) VALUES ('Can Food');
+INSERT INTO Categories (Name) VALUES ('Beverages');
+INSERT INTO Categories (Name) VALUES ('Starch');
+INSERT INTO Categories (Name) VALUES ('Meat');
+INSERT INTO Categories (Name) VALUES ('Toiletries');
+INSERT INTO Categories (Name) VALUES ('Fruits');
+INSERT INTO Categories (Name) VALUES ('Candy');
+INSERT INTO Categories (Name) VALUES ('Extras');
+
---
+++ nelisa_test.py
@@ -0,0 +1,6 @@
+
+
+import csv            # Imports the math module
+everything = dir(csv) # Sets everything to a list of things from math
+print everything       # Prints 'em all!
+
