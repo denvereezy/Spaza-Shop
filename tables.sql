094f81b19e1f2464ac223715c238b5d9bae9dad5
denver <denver@projectcodex.co>
2015-09-25 11:24:34 +0200

    updated database test using travis

--- tables.sql
+++ tables.sql
@@ -63,3 +63,11 @@ CREATE TABLE Purchases
 	FOREIGN KEY (Supplier_Id) REFERENCES Suppliers(Id),
 	FOREIGN KEY (Product_Id) REFERENCES Products(Id)
 );
+
+INSERT INTO Purchases (Qty,Purchase_price,Purchase_date,Product_Id, Supplier_Id)
+SELECT stock_purchases_csv.quantity,stock_purchases_csv.cost,stock_purchases_csv.date, Products.Id AS product_id, Suppliers.Id AS supplier_id
+FROM stock_purchases_csv 
+INNER JOIN Suppliers
+ON Suppliers.Name = stock_purchases_csv.shop
+INNER JOIN Products
+ON Products.Name = stock_purchases_csv.item;
