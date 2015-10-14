693c84771b95d7ea1cceca2ec6d5600a3e651150
denver <denver@projectcodex.co>
2015-10-14 07:46:54 +0200

    test update

--- test/travis-test.js
+++ test/travis-test.js
@@ -17,31 +17,31 @@ describe("Test mocha from Travis", function(){
   connection.connect();
     var queries = new Queries(connection);
 
-  it("should pass", function(done){
-
-    connection.query('select count(*) as userCount from users', function(err, users) {
-
-        console.log(err);
-
-        assert.equal(1, users[0].userCount);
-        done();
-    });
-
-
-  });
-
-//    it('should return supplier searched', function(done){
-//        var resultsCb = function(results){
-//            var ifExists = _.any(results, {'Name': 'HomeMade'});
-//            assert(ifExists);
-//            done();
-//        };
-//        queries.suppliers('Hom')
-//            .then(resultsCb)
-//            .catch(function(err){
-//            console.log(err);
-//        });
+//  it("should pass", function(done){
+//
+//    connection.query('select count(*) as userCount from users', function(err, users) {
+//
+//        console.log(err);
+//
+//        assert.equal(1, users[0].userCount);
+//        done();
 //    });
+//
+//
+//  });
+
+    it('should return supplier searched', function(done){
+        var resultsCb = function(results){
+            var ifExists = _.any(results, {'Name': 'HomeMade'});
+            assert(ifExists);
+            done();
+        };
+        queries.suppliers('Hom')
+            .then(resultsCb)
+            .catch(function(err){
+            console.log(err);
+        });
+    });
     
     it('should return a list of products containing "ea" ', function(done){
         var resultsCb = function(results){
