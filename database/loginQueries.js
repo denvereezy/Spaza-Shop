4b4ce30c410a507db5a0de9e032205e240e3204a
denver <denver@projectcodex.co>
2015-10-28 15:32:13 +0200

    test update

--- database/loginQueries.js
+++ database/loginQueries.js
@@ -14,15 +14,20 @@ module.exports = function(connection){
     };
     
     this.signup = function(data){
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(data.Password, salt, function(err, hash) {
-                  if (err){
-                      return console.log(err);
-                  }
-                  data.Password = hash;
-                 return executeQuery('insert into Users set ?', data);
-              });
-          });          
+        return new Promise(function(resolve,reject){
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(data.Password, salt, function(err, hash) {
+                    if (err){
+                        return console.log(err);
+                    }
+                    data.Password = hash;
+                    connection.query('insert into Users set ?', data, function(err, results) {
+                        if (err) return reject (err);
+                        resolve(results);
+                    });
+                });
+            });          
+        });
     };
     
     this.login = function(username){
